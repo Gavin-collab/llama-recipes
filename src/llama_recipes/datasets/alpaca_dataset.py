7824f6453ca7fd7d2fa7e2d692d0ae4211d80cd3
Gavin-collab <55814591+Gavin-collab@users.noreply.github.com>
2023-10-03 15:16:01 +0800

    Update alpaca_dataset.py

--- src/llama_recipes/datasets/alpaca_dataset.py
+++ src/llama_recipes/datasets/alpaca_dataset.py
@@ -9,6 +9,7 @@ import json
 import torch
 from torch.utils.data import Dataset
 
+# a map containing keys and tuples
 
 PROMPT_DICT = {
     "prompt_input": (
@@ -25,6 +26,8 @@ PROMPT_DICT = {
 
 class InstructionDataset(Dataset):
     def __init__(self, dataset_config, tokenizer, partition="train", max_words=30):
+
+        # ann is a json : loaded from a dataset file
         self.ann = json.load(open(dataset_config.data_path))
         if partition == "train":
             self.ann = self.ann
@@ -39,35 +42,60 @@ class InstructionDataset(Dataset):
     def __len__(self):
         return len(self.ann)
 
+    # method to get a single element at certain index
+    
     def __getitem__(self, index):
         IGNORE_INDEX = -100  # The default setting in CrossEntropyLoss
 
-
+        # get an element from ann which is a json
         ann = self.ann[index]
+
+        # if something has input
         if ann.get("input", "") == "":
+            # extract from the json and the json have keys that can be filled into the formatted string
             prompt = PROMPT_DICT["prompt_no_input"].format_map(ann)
         else:
             prompt = PROMPT_DICT["prompt_input"].format_map(ann)
+
+        # join the formatted prompt and the output which could be the label
         example = prompt + ann["output"]
+
+        # encode the prompt string to tensor using the tokenizer
         prompt = torch.tensor(
             self.tokenizer.encode(prompt), dtype=torch.int64
         )
+
+        # encode the example joint stirng to a sequence of tokens
         example = self.tokenizer.encode(example)
+
+        # put an end to the sequence of tokens
         example.append(self.tokenizer.eos_token_id)
+
+        # convert the sequence of tokens to tensor
         example = torch.tensor(
             example, dtype=torch.int64
         )
+
+        # pad the sequence to a specific length
         padding = self.max_words - example.shape[0]
         if padding > 0:
             example = torch.cat((example, torch.zeros(padding, dtype=torch.int64) - 1))
         elif padding < 0:
             example = example[: self.max_words]
+        # copy the example string
         labels = copy.deepcopy(example)
+
+        # convert the prompt part into -1
         labels[: len(prompt)] = -1
+        # get the indices - true/false where the element is 0
         example_mask = example.ge(0)
+        # do thd same in labels
         label_mask = labels.ge(0)
+        # convert the 0 indices to 0 in data
         example[~example_mask] = 0
+        # convert the 0 indices to IGNORE_INDEX in labels
         labels[~label_mask] = IGNORE_INDEX
+        
         example_mask = example_mask.float()
         label_mask = label_mask.float()
 
